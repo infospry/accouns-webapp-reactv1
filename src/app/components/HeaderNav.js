32e60f868b069d116a759b0d90520ad813845b5d
Basu <bd@infospry.com>
2023-09-07 18:57:13 +0530

    header

--- src/app/components/HeaderNav.js
+++ src/app/components/HeaderNav.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import Link from "next/link";
+
 
 const HeaderNav = () => {
   return (
@@ -8,12 +8,12 @@ const HeaderNav = () => {
     <>
 
       <nav className="navbar navbar-expand-xl mb-0 navbar-dark fixed-top">
-        <Link className="navbar-brand ms-2 me-3" href="/dashboard"><img src="images/logo.png" alt="logo" /></Link>
+        <a className="navbar-brand ms-2 me-3" href="#"><img src="images/logo.png" alt="logo" /></a>
         <div className="d_display">
           <div className="dropdown nav-item displinl ">
-            <Link href="javascript:void(0);" className="nav-link text-right dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+            <a href="javascript:void(0);" className="nav-link text-right dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
 
-              <span className="EmployeeAvatar">A</span></Link>
+              <span className="EmployeeAvatar">A</span></a>
             <div className="dropdown-menu p-0 bg-gradient-secondary">             
               <div className="user-header">
                 <img className="img-thumbnail rounded-circle profileImgBig" alt="User Image"
@@ -26,15 +26,15 @@ const HeaderNav = () => {
               </div>          
               <div className="user-body bg-secondary p-1">
                 <div>
-                  <Link className="btn btn-light btn-sm" data-toggle="modal" data-target="#password">Change Password</Link>
+                  <a className="btn btn-light btn-sm" data-toggle="modal" data-target="#password">Change Password</a>
                 </div>
               </div>
               <div className="user-footer text-center p-1 pb-2 pt-1 d-flex justify-content-center">
                 <div>
-                  <Link href="/settings" className="btn btn-primary me-2 btn-sm">Profile</Link>
+                  <a href="settings.html" className="btn btn-primary me-2 btn-sm">Profile</a>
                 </div>
                 <div>
-                  <Link href="/logout" className="btn btn-outline-danger btn-sm">Sign out</Link>
+                  <a href="/logout" className="btn btn-outline-danger btn-sm">Sign out</a>
                 </div>
               </div>
             </div>
@@ -47,23 +47,46 @@ const HeaderNav = () => {
         </div>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav m-auto">
-            <li className="nav-item active"><Link className="nav-link" href="/dashboard"> <i className='zmdi zmdi-desktop-windows'></i> Dashboard</Link></li>
+            <li className="nav-item active"><a className="nav-link" href="dashboard.html"> <i className='zmdi zmdi-desktop-windows'></i> Dashboard</a></li>
            
-            <li className="nav-item"><Link className="nav-link" href="/">   <i className='zmdi zmdi-graduation-cap'></i> Clients</Link></li>
-           <li className="nav-item"><Link className="nav-link" href="/"> <i className='zmdi zmdi-pizza'></i> Expances</Link></li>
-            <li className="nav-item"><Link className="nav-link" href="/"><i className='zmdi zmdi-receipt'></i> Invoices</Link></li>
-            <li className="nav-item"><Link className="nav-link" href="/"><i className='zmdi zmdi-folder'></i> Projects</Link></li>
-            <li className="nav-item"><Link className="nav-link" href="/reports"><i className='zmdi zmdi-trending-up'></i> Reports</Link></li>
-            <li className="nav-item"><Link className="nav-link" href="/"><i className='zmdi zmdi-comments'></i> Quotes</Link></li>
-            <li className="nav-item"><Link className="nav-link" href="/"><i className='zmdi zmdi-settings'></i> Settings</Link></li>
-            <li className="nav-item"><Link className="nav-link" href="/"><i className='zmdi zmdi-accounts-alt'></i> Team</Link></li>
-            <li className="nav-item"><Link className="nav-link" href="/"><i className='zmdi zmdi-widgets'></i> Vendor</Link></li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown" aria-expanded="false">
+               <i className='zmdi zmdi-graduation-cap'></i> Clients
+              </a>
+              <div className="dropdown-menu">
+                <a className="dropdown-item" href="overview.html">Overview</a>
+                <a className="dropdown-item" href="sales.html">All Sales</a>
+                <a className="dropdown-item" href="items.html"> Item</a>
+                <a className="dropdown-item" href="contacts.html"> Contacts</a>
+                <a className="dropdown-item" href="invoices.html"> Invoices</a>
+                <a className="dropdown-item" href="Estimates.html"> Estimates</a>
+                <a className="dropdown-item" href="expanses.html"> Expanses</a>
+                <a className="dropdown-item" href="expanses_category.html"> Expanses Category</a>
+                <a className="dropdown-item" href="Currencies.html"> Currencies and Units</a>
+              </div>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown" aria-expanded="false">
+              <i className='zmdi zmdi-pizza'></i> Expances
+              </a>
+              <div className="dropdown-menu">
+                <a className="dropdown-item" href="employes.html">Employee Dashboard</a>
+                <a className="dropdown-item" href="employee_list.html">Employee</a>
+              </div>
+            </li>
+            <li className="nav-item"><a className="nav-link" href="sales-pipline.html"><i className='zmdi zmdi-receipt'></i> Invoices</a></li>
+            <li className="nav-item"><a className="nav-link" href="projects.html"><i className='zmdi zmdi-folder'></i> Projects</a></li>
+            <li className="nav-item"><a className="nav-link" href="reports.html"><i className='zmdi zmdi-trending-up'></i> Reports</a></li>
+            <li className="nav-item"><a className="nav-link" href="event_calendar.html"><i className='zmdi zmdi-comments'></i> Quotes</a></li>
+            <li className="nav-item"><a className="nav-link" href="settings.html"><i className='zmdi zmdi-settings'></i> Settings</a></li>
+            <li className="nav-item"><a className="nav-link" href="activites.html"><i className='zmdi zmdi-accounts-alt'></i> Team</a></li>
+            <li className="nav-item"><a className="nav-link" href="activites.html"><i className='zmdi zmdi-widgets'></i> Vendor</a></li>
           </ul>
           <ul className="nav navbar-nav floatmargint">
             <li className="dropdown nav-item displinl ">
-              <Link href="javascript:void(0);" className="nav-link text-right dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+              <a href="javascript:void(0);" className="nav-link text-right dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
 
-                <span className="EmployeeAvatar">A</span></Link>
+                <span className="EmployeeAvatar">A</span></a>
               <div className="dropdown-menu p-0 bg-gradient-secondary">
                
                 <div className="user-header">
@@ -78,22 +101,22 @@ const HeaderNav = () => {
              
                 <div className="user-body bg-secondary p-1">
                   <div>
-                    <Link className="btn btn-light btn-sm" data-toggle="modal" data-target="#password">Change Password</Link>
+                    <a className="btn btn-light btn-sm" data-toggle="modal" data-target="#password">Change Password</a>
                   </div>
                 </div>
                 <div className="user-footer text-center p-1 pb-2 pt-1 d-flex justify-content-center">
                   <div>
-                    <Link href="/settings" className="btn btn-primary me-2 btn-sm">Profile</Link>
+                    <a href="settings.html" className="btn btn-primary me-2 btn-sm">Profile</a>
                   </div>
                   <div>
-                    <Link href="/logout" className="btn btn-outline-danger btn-sm">Sign out</Link>
+                    <a href="/logout" className="btn btn-outline-danger btn-sm">Sign out</a>
                   </div>
                 </div>
               </div>
             </li>
             <li className="nav-item float-right">
-              <Link className=" btn btn-primary btn-icon float-right right_icon_toggle_btn" href="#">
-                <i className="zmdi zmdi-arrow-right"></i></Link>
+              <a className=" btn btn-primary btn-icon float-right right_icon_toggle_btn" href="#">
+                <i className="zmdi zmdi-arrow-right"></i></a>
             </li>
           </ul>
         </div>
