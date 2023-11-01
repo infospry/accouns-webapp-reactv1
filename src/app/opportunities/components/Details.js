d214d06fcf589a73fe76e15e2b40c214c4ae46d8
Admin <ak@infospry.com>
2023-11-01 15:50:59 +0530

    Push

--- src/app/opportunities/components/Details.js
+++ src/app/opportunities/components/Details.js
@@ -39,13 +39,13 @@ function Details({res,contact}) {
                                 <div className="col-lg-6">                                                    
                                     <h4 className="col-grey m-0 p-0"> <i className="zmdi zmdi-account me-1"></i>Name</h4>
                                           <p className="mb-2"> <span className="col-black"><b>
-                                              {contact.lead_name !== '' ? <>{contact.lead_name} &nbsp;</> : 'NA'}</b> [<b className="col-green font-13 ms-1">Active</b>] </span> </p>
+                                              {contact.lead_name !== '' ? <>{contact.lead_title}&nbsp;&nbsp;{contact.lead_name} &nbsp;</> : 'NA'}</b> [<b className="col-green font-13 ms-1">Active</b>] </span> </p>
                                     
-                                    <h4 className="col-grey m-0 p-0"><i className="zmdi zmdi-accounts me-1"></i> Designations </h4>
-                                    <p className="mb-2"> <span className="col-black">Director</span> </p>
+                                    <h4 className="col-grey m-0 p-0"><i className="zmdi zmdi-accounts me-1"></i> Gender </h4>
+                                    <p className="mb-2"> <span className="col-black"> {contact.lead_gender !==''?<>{ contact.lead_gender}</> :'NA'} </span> </p>
 
-                                    <h4 className="col-grey m-0 p-0"><i className="zmdi zmdi-pin me-1"></i>Location </h4>
-                                    <p className="mb-2"> <span className="col-black">California</span> </p>
+                                    <h4 className="col-grey m-0 p-0"><i className="zmdi zmdi-calendar">&nbsp;</i>Date of Birth </h4>
+                                    <p className="mb-2"> <span className="col-black">{contact.lead_dob !==''?<>{ contact.lead_dob}</> :'NA'}</span> </p>
 
                                 </div>
                                 <div className="col-lg-6">
@@ -71,7 +71,9 @@ function Details({res,contact}) {
                 <div className="css-ra8ckx">
                     <div className="css-1waj6v4">
                         <h3 className="css-tixvpt"> Contact Address</h3>
-                        <span className="css-jcj5hu">Highlight your  Contact Information.</span>
+                              <span className="css-jcj5hu">{
+                                  contact.lead_address !== '' ? <>{contact.lead_address}</> : 'NA'}
+                              </span>
                     </div>
                     <svg xmlns="http://www.w3.org/2000/svg" focusable="false" role="img" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true" className="css-169u4i0">
                         <path
@@ -88,26 +90,25 @@ function Details({res,contact}) {
                         <div class="css-1waj6v4 eu4oa1w0">
                             <div class="row justify-content-center align-items-top">
                                 <div className="col-lg-6">                                                    
-                                    <h4 className="col-grey m-0 p-0"> <i className="zmdi zmdi-account me-1"></i>Name</h4>
-                                    <p className="mb-2"> <span className="col-black"><b>Nicole Pearson</b> [<b className="col-green font-13 ms-1">Active</b>] </span> </p>
+                                    <h4 className="col-grey m-0 p-0"> <i className="zmdi zmdi-pin me-1"></i>Address Line</h4>
+                                    <p className="mb-2"> <span className="col-black"><b>{contact.lead_address !==''?<>{ contact.lead_address}</> :'NA'}</b> </span> </p>
                                     
-                                    <h4 className="col-grey m-0 p-0"><i className="zmdi zmdi-accounts me-1"></i> Designations </h4>
-                                    <p className="mb-2"> <span className="col-black">Director</span> </p>
+                                      <h4 className="col-grey m-0 p-0"><i className="zmdi zmdi-pin me-1"></i> Country </h4>
+                                    <p className="mb-2"> <span className="col-black">{contact.country_name !==''?<>{ contact.country_name}</> :'NA'}</span> </p>
 
-                                    <h4 className="col-grey m-0 p-0"><i className="zmdi zmdi-pin me-1"></i>Location </h4>
-                                    <p className="mb-2"> <span className="col-black">California</span> </p>
+                                    <h4 className="col-grey m-0 p-0">Website </h4>
+                                    <p className="mb-2"> <span className="col-black">{contact.lead_website !==''?<>{ contact.lead_website}</> :''}</span> </p>
 
                                 </div>
                                 <div className="col-lg-6">
                                     
-                                    <h4 className="col-grey m-0 p-0"> <i className="zmdi zmdi-smartphone-android me-1"></i> Mobile</h4>
-                                    <p className="mb-2"> <span className="col-black">7123584758  <small className="col-green"> <i className="zmdi zmdi-check-all"> </i> Verified</small></span> </p>
+                                    <h4 className="col-grey m-0 p-0"> <i className="zmdi zmdi-pin me-1"></i> City/State</h4>
+                                    <p className="mb-2"> <span className="col-black"> {contact.lead_city !==''?<>{ contact.lead_city}</> :''} {contact.lead_county !==''?<>{ contact.lead_county}</> :''}</span> </p>
                                     
-                                    <h4 className="col-grey m-0 p-0"><i className="zmdi zmdi-phone me-1"></i> Phone </h4>
-                                    <p className="mb-2"> <span className="col-black">73737  <small className="col-green"> <i className="zmdi zmdi-check-all"> </i> Verified</small></span> </p>
+                                 
 
-                                    <h4 className="col-grey m-0 p-0"><i className="zmdi zmdi-email me-1"></i> Email</h4>
-                                    <p className="mb-2"> <span className="col-black">daniel.cook@gmail.com <small className="col-red"> <i className="zmdi zmdi-alert-triangle"> </i> Unerified</small></span> </p>
+                                    <h4 className="col-grey m-0 p-0"><i className="zmdi zmdi-pin me-1"></i> Postcode</h4>
+                                    <p className="mb-2"> <span className="col-black">{contact.lead_postcode !==''?<>{ contact.lead_postcode}</> :'NA'}</span> </p>
 
                                 </div>
                             </div>
