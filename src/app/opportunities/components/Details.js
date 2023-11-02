809bd5a0c013c31597bf5a7e18fad1d7e74de192
Basu <bd@infospry.com>
2023-11-02 13:36:44 +0530

    Sync

--- src/app/opportunities/components/Details.js
+++ src/app/opportunities/components/Details.js
@@ -20,7 +20,7 @@ function Details({res,contact}) {
                 <div className="accordion-item">
                     <h2 className="accordion-header">
                         <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-1">
-                            Information Details
+                            Basic Details
                         </button>
                     </h2>
                     <div id="faq-content-1" className="accordion-collapse collapse" data-bs-parent="#faqlist1">
@@ -56,13 +56,45 @@ function Details({res,contact}) {
                 </div>
                 <div class="accordion-item">
                     <h2 class="accordion-header">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq2-content-1">
-                        Where can I get some?
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-4">
+                        Contract Details
                         </button>
                     </h2>
-                    <div id="faqlist2-content-1" class="accordion-collapse collapse" data-bs-parent="#faqlist1">
-                        <div class="accordion-body">
-                            There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.
+                    <div id="faq-content-4" class="accordion-collapse collapse" data-bs-parent="#faqlist1">
+                    <div className="accordion-body">
+                            <div className="row justify-content-center align-items-top">
+                                <div className="col-lg-6">                                                    
+                                    <h4 className="col-grey m-0 p-0"> Reference Id</h4>
+                                        <p className="mb-2"> <span className="col-black"><b>
+                                            {contact.lead_name !== '' ? <>{contact.lead_title}&nbsp;&nbsp;{contact.lead_name} &nbsp;</> : 'NA'}</b> [<b className="col-green font-13 ms-1">Active</b>] </span> </p>
+                             
+                                    <h4 className="col-grey m-0 p-0"><i className="zmdi zmdi-calendar">&nbsp;</i>Title</h4>
+                                    <p className="mb-2"> <span className="col-black">{contact.lead_dob !==''?<>{ contact.lead_dob}</> :'NA'}</span> </p>
+                                    <h4 className="col-grey m-0 p-0"><i className="zmdi zmdi-phone me-1"></i> Estimated Value From </h4>
+                                    <p className="mb-2"> <span className="col-black">{contact.lead_phone !== '' ? <>{contact.lead_phone} &nbsp;</> : 'NA'}  <small className="col-green"> <i className="zmdi zmdi-check-all"> </i> Verified</small></span> </p>
+                                    <h4 className="col-grey m-0 p-0"><i className="zmdi zmdi-email me-1"></i> Deadline Date</h4>
+                                    <p className="mb-2"> <span className="col-black">  {contact.lead_email !== '' ? <>{contact.lead_email} &nbsp;</> : 'NA'} <small className="col-red"> <i className="zmdi zmdi-alert-triangle"> </i> Unerified</small></span> </p> 
+                                    <h4 className="col-grey m-0 p-0"><i className="zmdi zmdi-phone me-1"></i> Description </h4>
+                                    <p className="mb-2"> <span className="col-black">{contact.lead_phone !== '' ? <>{contact.lead_phone} &nbsp;</> : 'NA'}  <small className="col-green"> <i className="zmdi zmdi-check-all"> </i> Verified</small></span> </p>
+
+                                </div>
+                                <div className="col-lg-6">
+                                           
+                                <h4 className="col-grey m-0 p-0"><i className="zmdi zmdi-accounts me-1"></i> Opportunity Id </h4>
+                                    <p className="mb-2"> <span className="col-black"> {contact.lead_gender !==''?<>{ contact.lead_gender}</> :'NA'} </span> </p>
+
+                                    <h4 className="col-grey m-0 p-0"> <i className="zmdi zmdi-smartphone-android me-1"></i> Region of suppply</h4>
+                                    <p className="mb-2"> <span className="col-black">  {contact.lead_mobile !== '' ? <>{contact.lead_mobile} &nbsp;</> : 'NA'}  <small className="col-green"> <i className="zmdi zmdi-check-all"> </i> Verified</small></span> </p>
+                                    
+                                    
+                                    <h4 className="col-grey m-0 p-0"><i className="zmdi zmdi-email me-1"></i> Estimated Value To</h4>
+                                    <p className="mb-2"> <span className="col-black">  {contact.lead_email !== '' ? <>{contact.lead_email} &nbsp;</> : 'NA'} <small className="col-red"> <i className="zmdi zmdi-alert-triangle"> </i> Unerified</small></span> </p>
+
+                                    <h4 className="col-grey m-0 p-0"> <i className="zmdi zmdi-smartphone-android me-1"></i> Lead Suitable For</h4>
+                                    <p className="mb-2"> <span className="col-black">  {contact.lead_mobile !== '' ? <>{contact.lead_mobile} &nbsp;</> : 'NA'}  <small className="col-green"> <i className="zmdi zmdi-check-all"> </i> Verified</small></span> </p>
+                                    
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -75,6 +107,41 @@ function Details({res,contact}) {
                         </button>
                     </h2>
                     <div id="faq-content-2" className="accordion-collapse collapse" data-bs-parent="#faqlist1">
+                        <div className="accordion-body">
+                            <div className="row justify-content-center align-items-top">
+                                <div className="col-lg-6">                                                    
+                                    <h4 className="col-grey m-0 p-0"> <i className="zmdi zmdi-pin me-1"></i>Address Line</h4>
+                                    <p className="mb-2"> <span className="col-black"><b>{contact.lead_address !==''?<>{ contact.lead_address}</> :'NA'}</b> </span> </p>
+                                    
+                                    <h4 className="col-grey m-0 p-0"><i className="zmdi zmdi-pin me-1"></i> Country </h4>
+                                    <p className="mb-2"> <span className="col-black">{contact.country_name !==''?<>{ contact.country_name}</> :'NA'}</span> </p>
+
+                                    <h4 className="col-grey m-0 p-0">Website </h4>
+                                    <p className="mb-2"> <span className="col-black">{contact.lead_website !==''?<>{ contact.lead_website}</> :''}</span> </p>
+
+                                </div>
+                                <div className="col-lg-6">
+                                    
+                                    <h4 className="col-grey m-0 p-0"> <i className="zmdi zmdi-pin me-1"></i> City/State</h4>
+                                    <p className="mb-2"> <span className="col-black"> {contact.lead_city !==''?<>{ contact.lead_city}</> :''} {contact.lead_county !==''?<>{ contact.lead_county}</> :''}</span> </p>
+                                    
+                                
+
+                                    <h4 className="col-grey m-0 p-0"><i className="zmdi zmdi-pin me-1"></i> Postcode</h4>
+                                    <p className="mb-2"> <span className="col-black">{contact.lead_postcode !==''?<>{ contact.lead_postcode}</> :'NA'}</span> </p>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="accordion-item">
+                    <h2 className="accordion-header">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-5">
+                            Other Details
+                        </button>
+                    </h2>
+                    <div id="faq-content-5" className="accordion-collapse collapse" data-bs-parent="#faqlist1">
                         <div className="accordion-body">
                             <div className="row justify-content-center align-items-top">
                                 <div className="col-lg-6">                                                    
