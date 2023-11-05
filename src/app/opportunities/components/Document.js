0b93ba4ad11b62fc483d721a6f502551b03af021
Admin <ak@infospry.com>
2023-11-05 12:41:53 +0530

    Push

--- src/app/opportunities/components/Document.js
+++ src/app/opportunities/components/Document.js
@@ -39,7 +39,7 @@ function Document({ res=[] }) {
     </div>
     <div className="doclist">
         <div className="table-responsive mb-4">
-            <table className="table table-bordered overflow-y rwd-table btdr_none mb-3" id="sortable">
+            <table className="table table-bordered table-hover overflow-y rwd-table btdr_none mb-3" id="sortable">
                         <thead className="font_b dnone-mob">
                             <tr className="font_btd">
                                 <th style={{ width: "30px" }}>
@@ -47,21 +47,30 @@ function Document({ res=[] }) {
                                 </th>
                                 <th> File Name </th>
                                 <th>Folder</th>
-                                <th> Upload Date</th>
-                                <th>File Type</th>
+                               
+                              <th>File Type</th>
+                               <th> Upload Date</th>
                                 <th style={{width:'50px'}}>Action</th>
                             </tr>
-                        </thead>
+                      </thead>
+                    
                       <tbody>
                           
    {res && res.length > 0 && res[3].files_info != "" ? res && res.length > 0 && res[3].files_info.map((fl, i) => (
                                     <tr key={i} className="">
                                         <td className="">
                                             {fl.ROWNUMBER}
-                                        
-                                        </td>
+                                          <span className="dnnone">
+                                            {ext = fl.file_url.split('.')[1]}</span>
+           </td>
+           
+
+  
                                         <td className="pt-3 pb-3">
                                             
+               
+
+
                                             {ext === 'jpg' || ext === 'jpeg' ? <>
                                                 <span className="u_image float-left mr-2">
                                                     <img src="images/jpg.png" alt="user" className="rounded" width="36" />
@@ -92,12 +101,14 @@ function Document({ res=[] }) {
                                                                 </span>
                                                             </>
                                                                 : <></>
-                                            }<span className="float-left">
-                                                <span className="float-left col-blue"><b>{fl.file_title}</b></span><br />
-                                            </span>
+                                            }
+                                            <span class="float-left">
+                                           <span class="float-left nowrap-w220"><b>{fl.file_title}</b></span><br />
+                                          <small>File Size :- 5mb</small>
+                                          </span>
                                         </td>
-                                        <td>{"org"+fl.org_uid}</td>
-                                        <td className="col-grey"> {fl.upload_date}</td>
+                                         <td>{"org" + fl.org_uid}</td>
+                                      
                                         <td>{ext === 'jpg' || ext === 'jpeg' ? <>
                                         JPG File
                                             </>
@@ -119,13 +130,18 @@ function Document({ res=[] }) {
                                                </>
                                                :<>Some file</>
                                         }
-                                            </td>
+           </td>
+             <td className="col-grey"> {fl.upload_date}</td>
                                         <td style={{textAlign:'center'}}>
                                             <div className="btn-group">
                                                 <button type="button" className="btn btn-primary btn-sm dropdown-toggle dr-breakout-btn" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Action <span className="caret"></span></button>
-                   <ul className="dropdown-menu dr-breakout" style={{textAlign:'center'}}>
-                                                    <li className="btn btn-outline-primary btn-sm evt-leads-action" data-action="files" data-request_for="view" data-title={fl.file_title} data-file-name={fl.file_url} data-toggle="tooltip" title="View" > <i className="zmdi zmdi-eye"></i> View</li>
-                                                    &nbsp;<li className="btn btn-danger btn-sm evt-leads-action" data-u_id={fl.u_id} data-file-name={fl.file_url} data-action="files" data-request_for="delete" data-toggle="tooltip" title="Delete" type="button"> <i className="zmdi zmdi-delete"></i> Delete</li>
+                   <ul className="dropdown-menu dr-breakout" style={{ textAlign: 'center' }}>
+                       
+                       <li className="btn btn-outline-primary btn-sm evt-leads-action" data-action="files" data-request_for="view" data-title={fl.file_title} data-file-name={fl.file_url} data-toggle="tooltip" title="View" > <i className="zmdi zmdi-eye"></i> View</li>
+                        &nbsp;
+                       <li className="btn btn-outline-primary btn-sm evt-leads-action" data-action="files" data-request_for="view" data-title={fl.file_title} data-file-name={fl.file_url} data-toggle="tooltip" title="View"><i className="zmdi zmdi-download me-1"></i>Download</li>
+                       
+                                                    &nbsp;<li className="btn btn-danger btn-sm evt-leads-action" data-u_id={fl.u_id} data-file-name={fl.file_url} data-action="files" data-request_for="delete" data-toggle="tooltip" title="Delete" type="button"> <i className="zmdi zmdi-delete"></i></li>
                                                 </ul>
                                             </div>
                                         </td>
